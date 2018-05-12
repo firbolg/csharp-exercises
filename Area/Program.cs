1e83665c8539b3f1c530f58d7377b8998efa548c
levidolan <levijdolan@gmail.com>
2018-05-11 22:03:36 -0500

    completed Area

---
+++ Area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int r;
+            double A;
+            Console.WriteLine("Enter a radius:");
+            r = Convert.ToInt32(Console.ReadLine());
+            A = (3.14) * r * r;
+            Console.WriteLine("The area of the circle is " + A);
+            Console.ReadLine();
+                      
+        }
+    }
+}
